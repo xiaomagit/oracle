50a2816ab450baa5ef057e4487eb7a4f76f114e2
xiaoma <xiaoma@0620.com>
2019-06-19 17:01:06 +0800

    提交oracle数据库连接的代码

---
+++ oracle/src/test/java/com/itheima/oracle/OracleDemo.java
@@ -0,0 +1,28 @@
+package com.itheima.oracle;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class OracleDemo {
+
+    @Test
+    public void test() throws Exception {
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@192.168.184.10:1521:orcl", "scott", "tiger");
+        PreparedStatement ps = connection.prepareStatement("select * from emp where empno =?");
+        ps.setObject(1,7788);
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()){
+            System.out.println(rs.getString("ename"));
+        }
+
+        rs.close();
+        ps.close();
+        connection.close();
+    }
+
+}
